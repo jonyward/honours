99a3163bdce4dd863431f88192a2afd6f73dcd22
jonyward <jonyward@gmail.com>
2023-07-29 19:44:02 +0100

    Main Structure set up with settings.py configured, view set up as well as a templates folder, master.html file, and url.py for routing

--- crash_vis/views.py
+++ crash_vis/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
 
 # Create your views here.
