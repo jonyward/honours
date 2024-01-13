75f01a0010fa8e491a7a6b5f27f0633d28b01c97
jonyward <jonyward@gmail.com>
2024-01-13 14:31:16 +0000

    __str__ function added to models, debug mode turned off since everything works. Next step is to change nav bar to be horizontal rather than vertical to fit info at bottom of page. tailwind css potential front end library to give application better styling

--- crash_vis/models.py
+++ crash_vis/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Crashes (models.Model):
-
     Longitude = models.FloatField(("Longitude"))
     Latitude = models.FloatField(("Longitude"))
+
+    def __str__(self):
+        return '%s, %s' % (self.Longitude, self.Latitude)
+
 
 # Creates "Crashes" model, creating a table in the database which was filled with data from an external .csv file
--- crash_vis/tests.py
+++ crash_vis/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
 
- #This really needs done
+#This really needs done, really only needs to test whether or not the model migrations have worked, and that the table in the database contains the data, as well as checking
+#if pages are properly loading/maps being created in the view
